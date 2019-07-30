ef78db28107a7082a3329cab08b0150a30085f92
zxyle <zxyful@gmail.com>
2019-07-30 17:00:53 +0800

    more unit test

--- tests/test_uri.py
+++ tests/test_uri.py
@@ -9,26 +9,32 @@ from tinycrud.config import DEFAULT_REDIS_URI, DEFAULT_MONGODB_URI, DEFAULT_MYSQ
 from tinycrud.uri import UriParser
 
 
+LOCALHOST = ("localhost", "127.0.0.1")
+
+
 def test_mysql_uri():
     u = UriParser(DEFAULT_MYSQL_URI)
-    assert u.host in ("localhost", "127.0.0.1")
+    assert u.host in LOCALHOST
     assert u.port == 3306
     assert u.password == "123456"
     assert u.db == "test"
     assert u.user == "root"
+    assert u.scheme == "mysql+pymysql"
 
 
 def test_mongodb_uri():
     u = UriParser(DEFAULT_MONGODB_URI)
-    assert u.host in ("localhost", "127.0.0.1")
+    assert u.host in LOCALHOST
     assert u.password is None
     assert u.db == "test"
     assert u.user is None
+    assert u.scheme == "mongodb"
 
 
 def test_redis_uri():
     u = UriParser(DEFAULT_REDIS_URI)
-    assert u.host in ("localhost", "127.0.0.1")
+    assert u.host in LOCALHOST
     assert u.password is None
     assert int(u.db) == 0
     assert u.user is None
+    assert u.scheme == "redis"
