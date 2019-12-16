fceff7815974a892abb17f6325d3824e07c8040f
zxyle <zxyful@gmail.com>
2019-12-16 16:39:36 +0800

    Setup github Action mysql database

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+set -e
+# 查看mysql服务的状态，方便调试
+echo `service mysql status`
+echo '1.启动mysql'
+# 启动mysql
+service mysql start
+# 使进程休眠
+sleep 3
+echo `service mysql status`
+echo '2.开始导入数据'
+#导入sql文件
+mysql < /mysql/data.sql
+#echo '3.导入数据完毕....'
+#sleep 3
+#echo `service mysql status`
+## 由于最开始设置mysql为免密登陆，为了安全，在此设置mysql密码
+#echo '4.开始修改密码....'
+## 导入修改mysql权限设置的文件
+#mysql < /mysql/privileges.sql
+#echo '5.修改密码完毕....'
+#sleep 3
+#echo `service mysql status`
+#echo 'mysql容器启动完毕,且数据导入成功'
